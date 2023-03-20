f8f7ac04ecc668ee0c8a63a54e42bdada1e0f7f4
USERNAME <shinozakiyume88@gmail.com>
2023-03-20 09:46:50 +0900

    Inline Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import { InlineStyle } from "./composents/InlineStyle";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <InlineStyle />
     </div>
   );
 }
