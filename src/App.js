54ed82bd83bbf1b0edd9856e341ebd7981f6f7af
USERNAME <shinozakiyume88@gmail.com>
2023-03-20 10:56:03 +0900

    Styled JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { CssModules } from "./composents/CssModules";
 import { InlineStyle } from "./composents/InlineStyle";
+import { StyledJsx } from "./composents/StyledJsx";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
+      <StyledJsx />
     </div>
   );
 }
